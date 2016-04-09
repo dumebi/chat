07d9bd376f949abc2cf68da8c1bd6b993e8b7ddb
Devin Kuokka <devin.kuokka@wustl.edu>
2016-04-09 16:29:12 -0500

    no message

--- chat-server.js
+++ chat-server.js
@@ -19,8 +19,21 @@ app.listen(3456);
  
 // Do the Socket.IO magic:
 var io = socketio.listen(app);
+var rooms = [];
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
+	
+	socket.on("new_user", function(){
+		for (r in rooms){
+			io.sockets.emit("room_list", {room: r});
+		}
+	})
+	
+	socket.on("add_room", function(data) {
+		//console.log(data["user"] + ": " + data["message"]); // log it to the Node.JS output
+		rooms[rooms.length] = data['room'];
+		io.sockets.emit("new_room", {room: data['room']}) // broadcast the message to other users
+	});
  
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
